e136530bcc666c59e9b132ed0f987f64da927ba6
aaron <asouer@gmail.com>
2018-01-16 15:51:57 -0600

    resolves #3

--- guess_the_number.py
+++ guess_the_number.py
@@ -5,9 +5,15 @@ too_low = 'too low'
 too_high = 'too high'
 
 
-
 def configure_range(low=1, high=10):
     '''Set the high and low values for the random number'''
+    return ask_range()
+
+
+def ask_range():
+    """Asks the user for a range of numbers"""
+    low = ask_low()
+    high = ask_high(low)
     return low, high
 
 
@@ -16,23 +22,48 @@ def generate_secret(low, high):
     return random.randint(low, high)
 
 
-def get_guess():
+def get_guess(low=1, high=10):
     '''get user's guess'''
-    return validate_guess()
+    return validate_guess(low, high)
+
+
+def ask_low():
+    low = -1
+    while low < 0:
+        numb = input("Please enter an integer grater than 0 for the lower number: ")
+        if is_number(numb) and int(numb) > 0:
+            low = int(numb)
+    return low
+
+
+def ask_high(low):
+    high = -1
+    while high < low:
+        numb = input("Please enter an integer grater than {} for the higher number: ".format(low + 10))
+        if is_number(numb) and int(numb) > low + 10:
+            high = int(numb)
+    return high
+
+
+def is_number(numb):
+    try:
+        int(numb)
+        return True
+    except ValueError:
+        return False
 
 
 def validate_guess(low=1, high=10):
     """validates the guess """
     guess = -1
     while guess < 0:  # chick if guess is valid
-        str_guess = input('Guess the secret number? ')
-        try:
+        str_guess = input('Guess the secret number? (between {} and {})'.format(low, high))
+        if is_number(str_guess):
             guess = int(str_guess)
             if not low <= guess <= high:
                 guess = -1
-        except ValueError:
+        else:
             guess = -1
-            continue
     return int(guess)
 
 
@@ -53,7 +84,7 @@ def main():
 
     while True:
         guesses += 1
-        guess = get_guess()
+        guess = get_guess(low, high)
         result = check_guess(guess, secret)
         print(result)
 
